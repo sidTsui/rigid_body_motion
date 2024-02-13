54c29cfe1f34d3020a570a3e5528cab0e63beb23
sidTsui <147187017+sidTsui@users.noreply.github.com>
2024-02-12 21:53:10 -0500

    finalized p3_sol.py

--- scripts/p3_sol.py
+++ scripts/p3_sol.py
@@ -10,6 +10,7 @@ def H_1():
 	translationy = p2_sol.transformy(-1.5)#trans -1.5 on y
 	
 	result = translationx @ translationz @ translationy #calculate matrices product
+	return result
 
 def H_2():
 	#current axis'
@@ -18,27 +19,41 @@ def H_2():
 	translationy = p2_sol.transformy(-1.5)#trans of -1.5 on y
 	
 	result = translationz @ translationx @ translationy #calculate mult matrices
+	return result
 
 def H_3():
 	#fixed frame
-	translationy = p2_sol.transformy(-1.5)
-	translationz = p2_sol.transformz(0.5)
-	translationx = p2_sol.transformx(2.5)
+	translationy = p2_sol.transformy(1.5)
+	translationz = p2_sol.transformz(-0.5)
+	translationx = p2_sol.transformx(-2.5)
 
 	result = translationy @ translationz @ translationx #multiply inverse
-
+	return result
+	
 def H_4():
 	#fixed frame
-	translationy = p2_sol.transformy(-1.5)
-	translationx = p2_sol.transformx(2.5)
-	translationz = p2_sol.transformz(0.5)
+	translationy = p2_sol.transformy(1.5)
+	translationx = p2_sol.transformx(-2.5)
+	translationz = p2_sol.transformz(-0.5)
 	
 	result = translationy @ translationx @ translationz#mult inverse
+	return result
 
 def H_5():
-	rotationx = p2_sol.rotx(math.pi/2) #rotate x at pi/2
+	rotationx = p2_sol.rotationx(math.pi/2) #rotate x at pi/2
 	translationx = p2_sol.transformx(3.0) #trans x by 3.0
 	translationz = p2_sol.transformz(-3.0) #trans z by 3.0
-	rotationz = p2_sol.rotz(-(math.pi/2)) #rotate z by -pi/2
+	rotationz = p2_sol.rotationz(-(math.pi/2)) #rotate z by -pi/2
 
 	result = rotationx @ translationx @ translationz @ rotationz
+	return result
+	
+print(H_1())
+print("")
+print(H_2())
+print("")
+print(H_3())
+print("")
+print(H_4())
+print("")
+print(H_5())
